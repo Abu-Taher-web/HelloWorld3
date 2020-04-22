4ba1229cc7edd7ab985d80e48fcf62d3d2edd820
Abu Taher <Abu-Taher-web@users.noreply.github.com>
2020-04-22 09:55:32 +0600

    Add project files.

---
+++ HelloWorld3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HelloWorld3
+{
+
+
+
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+
+                var number = "1234";
+                byte b = Convert.ToByte(number);
+                Console.WriteLine(b);
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("The number couldnot be converter...");
+                
+            }
+          
+        }
+    }
+}
